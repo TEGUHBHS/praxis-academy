b7799c33f72601f825ea4e2de405948951b83c80
unknown <teguhbhs@gmail.com>
2019-12-05 10:33:11 +0700

    hari4

--- kemampuan-dasar/kemampuan-dasar-1/latihan/latihan class.py
+++ kemampuan-dasar/kemampuan-dasar-1/latihan/latihan class.py
@@ -1,12 +1,14 @@
 class Person:
-    def __init__(self, name):
+    def __init__(self, name, alamat, nohp):
         self.name = name
         self.alamat = alamat
         self.nohp = nohp
-        
+
 
     def say_hi(self):
         print('Hello, my name is', self.name)
+        print('rumahku', self.alamat)
+        print('no hp', self.nohp)
 
 p = Person('Swaroop')
 p.say_hi()
